289134d3e084f3194a856870d5594fae9094e518
amikoelvis <amikoelvis@gmail.com>
2024-06-21 04:04:03 -0700

    "Programming is like building a multilingual puzzle, followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* prints exactly "Programming is like building a multilingual puzzle */
+
+/**
+ * main - Entry point
+ * puts - prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
